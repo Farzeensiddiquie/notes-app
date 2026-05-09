141e9843c0e0bafa5a34d3fd4fc080e3e38208bf
Farzeensiddiquie <farzeenwasif15@gmail.com>
2026-05-09 12:23:46 +0500

    changed data files

--- components/NotesContainer.js
+++ components/NotesContainer.js
@@ -1,56 +1,10 @@
 // NotesContainer.jsx
 import { View } from "react-native";
 import NotesCard from "./NotesCard";
-
+import { FlatList } from "react-native-gesture-handler";
+import notesData from "../data/notes.json";
 export default function NotesContainer() {
-    const colors = [
-    "bg-blue-300",
-    "bg-purple-300",
-    "bg-pink-300",
-    "bg-green-300",
-    "bg-yellow-300",
-    "bg-orange-300",
-  ];
-  const notes = [
-    {
-      id: 1,
-      title: "Meeting Notes - Team Sync Oct 15, 2023",
-      desc: "Notes from the weekly team meeting.",
-      date: "Oct 15, 2023",
-    },
-    {
-      id: 2,
-      title: "Project Planning",
-      desc: "Discuss app structure, API integration and deadlines.",
-      date: "Nov 2, 2023",
-    },
-    {
-      id: 3,
-      title: "UI Improvements",
-      desc: "Need smoother animations and responsive layout fixes.",
-      date: "Dec 8, 2023",
-    },
-    {
-      id: 4,
-      title: "Client Feedback",
-      desc: "Update dashboard layout and add loading states.",
-      date: "Jan 4, 2024",
-    },
-     {
-      id: 5,
-      title: "Client Feedback",
-      desc: "Update dashboard layout and add loading states.",
-      date: "Jan 4, 2024",
-    },
-     {
-      id: 6,
-      title: "Client Feedback",
-      desc: "Update dashboard layout and add loading states.",
-      date: "Jan 4, 2024",
-    },
-  ];
-
-  return (
+    return (
     <View className="flex-row flex-wrap justify-center px-4 mb-20 mt-4">
       {notes.map((item, index) => (
         <View
@@ -60,12 +14,19 @@ export default function NotesContainer() {
             ${index % 2 !== 0 ? "-ml-[60px] mt-[40px] z-10" : ""}
           `}
         >
-          <NotesCard
-            title={item.title}
-            desc={item.desc}
-            date={item.date}
-            color={colors[index % colors.length]}
-          />
+         <FlatList
+  data={notesData}
+  keyExtractor={(item) => item.id.toString()}
+  numColumns={2}
+  renderItem={({ item, index }) => (
+    <NotesCard
+      title={item.title}
+      desc={item.desc}
+      date={item.date}
+      color={colors[index % colors.length]}
+    />
+  )}
+/>
         </View>
       ))}
     </View>
