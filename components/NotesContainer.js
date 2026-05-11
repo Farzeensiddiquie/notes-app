7c1a9cfde2d9e277d90f3435d43dbdea8f511997
Farzeensiddiquie <farzeenwasif15@gmail.com>
2026-05-11 13:45:54 +0500

    commit

--- components/NotesContainer.js
+++ components/NotesContainer.js
@@ -4,7 +4,7 @@ import notesData from "../data/notes.json";
 
 export default function NotesContainer() {
   return (
-    <View className="px-4 mt-4 mb-[150px]">
+    <View className="px-4 mt-4 mb-[150px] self-center">
       <FlatList
         data={notesData}
         keyExtractor={(item) => item.id.toString()}
@@ -16,8 +16,8 @@ export default function NotesContainer() {
         renderItem={({ item, index }) => (
           <View
             className={`
-              w-[48%]
-              ${index % 2 !== 0 ? "-ml-[60px] mt-[40px] z-10" : ""}
+              
+              ${index % 2 !== 0 ? "-ml-[70px] mt-[40px] z-10" : ""}
             `}
           >
             <NotesCard
